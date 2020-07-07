36931e0b65c20f1e4cfb67e6b2b8df6e283e0344
dylan.k <bee2side@gmail.com>
2020-07-08 08:49:27 +0900

    swipe

--- IOSPageControlTutorial/ViewController.swift
+++ IOSPageControlTutorial/ViewController.swift
@@ -8,10 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    var movies: [String] = ["bad-boys", "joker", "hollywood"]
+    var frame = CGRect.zero
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    func setupScreens() {
+        for index in 0..<movies.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: movies[index])
+            
+            self.scrollView.addSubview(imgView)
+        }
+        
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(movies.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pageControl.numberOfPages = movies.count
+        setupScreens()
+        
+        scrollView.delegate = self
         // Do any additional setup after loading the view.
     }
 
